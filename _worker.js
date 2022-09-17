190d968be08bd899bb7362e0c83676c6aa137e4b
banshicy <banshiyc@126.com>
2022-09-17 12:35:31 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="91.215.152.170:8080";
+        url.hostname="91.215.152.170";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
